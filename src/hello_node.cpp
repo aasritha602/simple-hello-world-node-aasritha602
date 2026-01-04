ffb661b19ca5758bbe03d1e42fd1bb5af0678f12
Aasritha Koganti <aasrithakoganti909@gmail.com>
2026-01-04 14:13:56 +0530

    Update hello_node.cpp

--- src/hello_node.cpp
+++ src/hello_node.cpp
@@ -7,6 +7,33 @@
 
 using namespace std::chrono_literals;
 
+class HelloNode : public rclcpp::Node
+{
+public:
+  HelloNode()
+  : Node("hello_world_node")
+  {
+     timer_=this->create_wall_timer(
+       1000ms,
+      std::bind (&HelloNode::timer_callback, this)
+   );
+}
+private:
+  void timer_callback()
+  {
+    RCLCPP_INFO(this->get_logger(), "Hello, World!");
+  }
+
+  rclcpp::TimerBase::SharedPtr timer_;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<HelloNode>());
+  rclcpp::shutdown();
+  return 0;
+}
 /*
  * TODO: Create a Class named 'HelloNode' that inherits from rclcpp::Node.
  * Requirements:
